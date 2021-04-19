97f92fba964e38e50c2af719e98f42790c314daa
Hamed0406 <h.adeli.meadeli@gmail.com>
2021-04-19 15:06:44 +0200

    working on navigatoin bar

--- my-app/src/components/navigation/desktopnavigation/Desktopnavigation.tsx
+++ my-app/src/components/navigation/desktopnavigation/Desktopnavigation.tsx
@@ -1,10 +1,12 @@
 import './Desktopnavigation.css'
 import { useHistory } from 'react-router-dom'
+import logotype from '../../../shared/images/undraw_cooking_lyxy.svg'
 //for showing desktop navigation
 export const Desktopnavigation = () => {
     const history=useHistory()
     return (
-        <div>
+        <div className='desktopNaigationWrapper'>
+            <img className='navigationLogo' onClick={()=> history.push('/')} src={logotype} alt={'error...'} style={{width: 50}}/>
             <span onClick={()=> history.push('/')}  >test</span>
             <span onClick={()=> history.push('/home')}>home</span>
             <span onClick={()=> history.push('/gallery')}>gallery</span>
