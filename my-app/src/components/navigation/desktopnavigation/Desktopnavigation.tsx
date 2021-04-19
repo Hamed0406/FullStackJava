0bf702d291c73ff25b53f73f0215c84a20bc26d1
Hamed0406 <h.adeli.meadeli@gmail.com>
2021-04-19 15:22:38 +0200

    add bakground picture

--- my-app/src/components/navigation/desktopnavigation/Desktopnavigation.tsx
+++ my-app/src/components/navigation/desktopnavigation/Desktopnavigation.tsx
@@ -1,11 +1,12 @@
 import './Desktopnavigation.css'
 import { useHistory } from 'react-router-dom'
 import logotype from '../../../shared/images/undraw_cooking_lyxy.svg'
+import mainIMage from '../../../shared/images/body_M.jpg'
 //for showing desktop navigation
 export const Desktopnavigation = () => {
     const history=useHistory()
     return (
-        <div className='desktopNaigationWrapper'>
+        <div className='desktopNaigationWrapper'  >
             <img className='navigationLogo' onClick={()=> history.push('/')} src={logotype} alt={'error...'} style={{width: 50}}/>
             <span onClick={()=> history.push('/')}  >test</span>
             <span onClick={()=> history.push('/home')}>home</span>
@@ -18,3 +19,4 @@ export const Desktopnavigation = () => {
         </div>
     )
 }
+
