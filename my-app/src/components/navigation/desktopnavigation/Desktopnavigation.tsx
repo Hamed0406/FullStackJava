99cf901c699032261bebded01850bdac77fc478b
Hamed0406 <h.adeli.meadeli@gmail.com>
2021-04-20 09:47:22 +0200

    Add CSS style to DesktopNavigation

--- my-app/src/components/navigation/desktopnavigation/Desktopnavigation.tsx
+++ my-app/src/components/navigation/desktopnavigation/Desktopnavigation.tsx
@@ -1,22 +1,27 @@
 import './Desktopnavigation.css'
 import { useHistory } from 'react-router-dom'
 import logotype from '../../../shared/images/undraw_cooking_lyxy.svg'
-import mainIMage from '../../../shared/images/body_M.jpg'
 //for showing desktop navigation
 export const Desktopnavigation = () => {
-    const history=useHistory()
+    const history = useHistory()
     return (
         <div className='desktopNaigationWrapper'  >
-            <img className='navigationLogo' onClick={()=> history.push('/')} src={logotype} alt={'error...'} style={{width: 50}}/>
-            <span onClick={()=> history.push('/')}  >test</span>
-            <span onClick={()=> history.push('/home')}>home</span>
-            <span onClick={()=> history.push('/gallery')}>gallery</span>
-            <span onClick={()=> history.push('/signIn')}>signIn</span>
-            <span onClick={()=> history.push('/singUp')}>singUp</span>
-            <span onClick={()=> history.push('/news')}>news</span>
-            <span onClick={()=> history.push('/shop')}>shop</span>
+
+
+            <img className='navigationLogo' onClick={() => history.push('/')} src={logotype} alt={'error...'} />
+            <div className='otherLinks'>
+            <span className='signUpButton' onClick={() => history.push('/singUp')}>singUp</span>
+            <span  className='newsButton' onClick={() => history.push('/news')}>news</span>
+            <span className='shopButton' onClick={() => history.push('/shop')}>shop</span>
+            <span className='galleryButton' onClick={() => history.push('/gallery')}>gallery</span>
+            </div>
+            <span className='singIn' onClick={() => history.push('/signIn')}>signIn</span>
+
+
+
 
         </div>
+
     )
 }
 
