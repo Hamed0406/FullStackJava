2c8f9b0c6782f48ae45dc58dc10ef26b38c4906d
Hamed0406 <h.adeli.meadeli@gmail.com>
2021-04-21 10:08:06 +0200

    useefect

--- my-app/src/views/navigationViews/SignInView.tsx
+++ my-app/src/views/navigationViews/SignInView.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { useState } from 'react'
+import {useEffect} from 'react'
 
 export const SignInView = () => {
+    const [userName, setUserName] = useState<String>('')
+    const [password,setPassword]=useState<String>('')
+
     return (
         <div>
-            <h1>Sign in view</h1>
+            <h1>{userName}</h1>
+            <h1>{password}</h1>
+            <span>UserName : </span>
+            <input onChange={event=> setUserName(event.target.value)} /><br/>
+            <span>Password</span>           
+             <input  onChange={event=> setPassword(event.target.value)} /><br/>
+
+            <button onClick={() => setUserName('Random')} >Login</button>
         </div>
     )
 }
