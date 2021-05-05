96a7fae40d2f66242f605cea9b8c8cdf50c7fe7c
Hamed0406 <h.adeli.meadeli@gmail.com>
2021-05-05 10:36:54 +0200

    15 April

--- src/components/navigation/desktopnavigation/Desktopnavigation.tsx
+++ src/components/navigation/desktopnavigation/Desktopnavigation.tsx
@@ -2,9 +2,13 @@ import './Desktopnavigation.css'
 import { useHistory } from 'react-router-dom'
 import logotype from '../../../shared/images/undraw_cooking_lyxy.svg'
 import RouttingPath from '../../../routes/RouttingPath'
+import {useContext,useState} from 'react'
+import { UserContext } from '../../../shared/provider/UserProvider'
+
 
 //for showing desktop navigation
 export const Desktopnavigation = () => {
+    const [authenticatorUser,setAuthenticatorUser]=useContext(UserContext)
     const history = useHistory()
     return (
         <div className='desktopNaigationWrapper'  >
@@ -16,9 +20,10 @@ export const Desktopnavigation = () => {
             <span  className='newsButton' onClick={() => history.push(RouttingPath.news)}>news</span>
             <span className='shopButton' onClick={() => history.push(RouttingPath.shop)}>shop</span>
             <span className='galleryButton' onClick={() => history.push(RouttingPath.gallery)}>gallery</span>
+            <span >{authenticatorUser}</span>
+
             </div>
             <span className='singIn' onClick={() => history.push(RouttingPath.signIn)}>signIn</span>
-
 
 
 
