0c2b43374fb350c11f906259a2ab5620e246016a
Hamed0406 <h.adeli.meadeli@gmail.com>
2021-05-12 20:06:44 +0200

    Add DB

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 12
+	},
+	'rules': {
+		'indent': [
+			'warn',
+			'tab'
+		],
+		'linebreak-style': [
+			'warn',
+			'windows'
+		],
+		'quotes': [
+			'warn',
+			'single'
+		],
+		'semi': [
+			'warn',
+			'never'
+		]
+	}
+}
--- backend/Server.js
+++ backend/Server.js
@@ -1,27 +1,37 @@
-import express, { response } from 'express'
-import { request } from 'node:https'
+import express from 'express'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+dotenv.config()
+const {PORT}=process.env
 
 const application=express()
+application.use(morgan('common'))
 
-const isOrderHaddPaid=(request,response,next)=> {
-    console.log('Payment Verifed ')
-    next()
-
-}
-application.use(isOrderHaddPaid)
 
 application.get('/order',(request,response)=>
 {
     response.send('Order is created :'+Math.random())
-})
+}) 
 
 const notFound=(request,response,next)=>{
-    const error=new Error('Not Found: ${request.orgnilaUrl}')
+    const error=new Error(`Not Found: ${request.orgnilaUrl}`)
     response.status(404)
     next(error)
 }
 application.use(notFound)
 
-application.listen(3001,()=> {
-    console.log('Ther serve is up ans ronning on port 3001')
+application.listen(PORT,()=> {
+    console.log(` Ther serve is up ans ronning on port :${PORT}`)
+})
+
+mongoose.connect('mongodb://localhost/my_database',{ useNewUrlParser: true,
+useUnifiedTopology: true,
+useFindAndModify: false,
+useCreateIndex: true})
+.then(()=>console.log('Sucesssfully concetet to DB'))
+.catch((error)=> {
+    console.log('Error had been coured to connect to DB :'+error)
+    process.exit()
 })
