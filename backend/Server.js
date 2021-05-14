d2b1dffc49ea933afbf24c623ba15a89d0fd21dc
Hamed0406 <h.adeli.meadeli@gmail.com>
2021-05-14 20:00:53 +0200

    update

--- backend/Server.js
+++ backend/Server.js
@@ -1,19 +1,16 @@
 import express from 'express'
 import morgan from 'morgan'
+import helmet from 'helmet'
 import UserRoute from './src/routes/User.Route.js'
 import Configoration from './src/configurations/Configoration.js'
 import Midelwares from './src/midelwares/Midelwares.js'
 
-
+//Keep warpper Structed 
 const application=express()
 application.use(express.json())
 application.use(morgan('common'))
-
- 
-
+application.use(helmet())
 UserRoute.routes(application)
-
-
 application.use(Midelwares.notFound)
 Configoration.connectToPort(application)
 Configoration.connetToDB()
