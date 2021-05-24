2fb9d2b41ac351a7c8cfb1a6abc1364a9c0f661f
Hamed0406 <h.adeli.meadeli@gmail.com>
2021-05-24 19:43:28 +0200

    test

--- backend/test/User.test.js
+++ backend/test/User.test.js
@@ -1,4 +1,4 @@
-import chai from 'chai'
+import Chai from 'chai'
 import chaiHTTP from 'chai-http'
 import { response } from 'express'
 import {describe,it as test} from 'mocha'
@@ -8,13 +8,15 @@ import application from '../Server.js'
 Chai.should()
 Chai.use(chaiHTTP)
 
+const randomString=Math.random().toString(36).substring(7)
+
 //not found 
 
 const testNonExistingRoute=()=> {
-describe('API call non Existing route.404,not-found'),()=> {
-    test('Expecting 404 NOT_FOUND',done => {
+describe('API call non Existing route.404,not-found',()=> {
+    test('Expecting 404 NOT_FOUND',(done) => {
         Chai.request(application)
-        .get('/testhehaha')
+        .get(  `/${randomString}`)
         .end((request,response) => {
             response.should.have.a.status(404)
             done()
@@ -22,8 +24,10 @@ describe('API call non Existing route.404,not-found'),()=> {
     })
 
 }
- 
+)
 }
+
+
 
 describe('TESTING USER API ROUTE',()=>{
     testNonExistingRoute()
