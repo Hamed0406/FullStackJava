6d292c399b8750d1192eb96b5117cc32ddce00f1
Hamed0406 <h.adeli.meadeli@gmail.com>
2021-05-24 20:11:07 +0200

    update

--- backend/test/User.test.js
+++ backend/test/User.test.js
@@ -13,7 +13,6 @@ const randomString=Math.random().toString(36).substring(7)
 //not found 
 
 const testNonExistingRoute=()=> {
-describe('API call non Existing route.404,not-found',()=> {
     test('Expecting 404 NOT_FOUND',(done) => {
         Chai.request(application)
         .get(  `/${randomString}`)
@@ -23,12 +22,49 @@ describe('API call non Existing route.404,not-found',()=> {
         })
     })
 
+
+
 }
-)
+
+const getAllUsers= ()=> {
+    test('Fetch All User from DB Get ',(done) => {
+        Chai.request(application)
+        .get(  '/user')
+        .end((request,response) => {
+            response.should.have.a.status(200)
+            response.body.should.be.a('array')
+
+            done()
+        })
+    })
+}
+
+const createUser= ()=> {
+
+const mockData={
+
+    username: randomString,
+    password: randomString
+}
+
+    test('CREATE USER POST ',(done) => {
+        Chai.request(application)
+        .post(  '/user')
+        .send(mockData)
+        .end((request,response) => {
+            response.should.have.a.status(201)
+            response.body.should.be.a('object')
+            response.body.should.have.property('username').eq(mockData.username)
+            response.body.should.have.property('password').eq(mockData.password)
+            done()
+        })
+    })
 }
 
 
 
 describe('TESTING USER API ROUTE',()=>{
     testNonExistingRoute()
+    getAllUsers()
+    createUser()
 })
